8255ad449343c17ef615ea3bf664a32c36ef62f3
vesabraina <vesa.braina@student.uni-pr.edu>
2026-04-18 23:16:39 +0200

    added ClientHandler changes

--- src/Clienthandler.java
+++ src/Clienthandler.java
@@ -1,2 +1,29 @@
-public class Clienthandler {
-}
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class ClientHandler implements Runnable {
+
+    private Socket socket;
+    private BufferedReader in;
+    private PrintWriter out;
+    private boolean isAdmin = false;
+
+    public ClientHandler(Socket socket) throws Exception {
+        this.socket = socket;
+
+        socket.setSoTimeout(30000);
+
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        out = new PrintWriter(socket.getOutputStream(), true);
+
+        out.println(&quot;Enter username:&quot;);
+        String user = in.readLine();
+
+        if (user.equals(&quot;admin&quot;)) {
+            isAdmin = true;
+            out.println(&quot;You are admin&quot;);
+        } else {
+            out.println(&quot;You are normal user&quot;);
+        }
+    }
