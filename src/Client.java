37d6239e0b38c29774949282ec622e474f212f53
VesaXhelili1 <vesa.xhelili@student.uni-pr.edu>
2026-04-19 18:46:32 +0200

    add input and output streams for client-server communication

--- src/Client.java
+++ src/Client.java
@@ -4,5 +4,11 @@ public class Client {
     public static void main(String[] args) throws Exception {
 
     Socket socket = new Socket("localhost", 1234);
+
+
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(socket.getInputStream()));
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 }
  }
