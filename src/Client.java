de5ed2caa0ceddfd9558f291013d392c8e0e9ff1
VesaXhelili1 <vesa.xhelili@student.uni-pr.edu>
2026-04-19 18:44:43 +0200

    Initialize basic TCP client connection

--- src/Client.java
+++ src/Client.java
@@ -1,4 +1,8 @@
+import java.net.*;
+import java.io.*;
 public class Client {
-    import java.net.*;
-    import java.io.*;
+    public static void main(String[] args) throws Exception {
+
+    Socket socket = new Socket("localhost", 1234);
 }
+ }
