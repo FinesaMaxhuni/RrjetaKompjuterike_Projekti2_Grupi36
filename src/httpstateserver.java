49961884dbca738eaea4d72cce44740b97b11613
vesashala5 <vesa.shala5@student.uni-pr.edu>
2026-04-19 17:11:11 +0200

    import proper classes

--- src/httpstateserver.java
+++ src/httpstateserver.java
@@ -1,5 +1,12 @@
 import com.google.gson.Gson;
 
+import com.sun.net.httpserver.*;
+import java.io.*;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+import java.util.concurrent.Executors;
+
 public class HttpStatsServer {
 
     public static void start() throws Exception {
