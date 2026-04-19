bb1d2d589aa7be7f06d4af5e31555a46beb19d04
vesashala5 <vesa.shala5@student.uni-pr.edu>
2026-04-19 16:36:05 +0200

    initialize HTTP server on port 8080

--- src/httpstateserver.java
+++ src/httpstateserver.java
@@ -3,6 +3,7 @@ import com.google.gson.Gson;
 public class HttpStatsServer {
 
     public static void start() throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
 
     }
 }
